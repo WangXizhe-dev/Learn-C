2e76a9a5ba6c466a9325353272f0a12d65afb153
xizhe-wang <142483410+WangXizhe-dev@users.noreply.github.com>
2026-07-07 20:55:50 +0800

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -2,10 +2,12 @@
 
 int main()
 {
-    int a = 0;
-    int b = 0;
-    printf("请输入两个数字，中间用空格隔开，您将会得到两者之和");
-    scanf("%d %d",&a,&b);
-    printf("%d+%d=%d\n",a,b,a+b);
+    double a = 0 , b = 0;
+    printf("请输入两个数字，中间用空格隔开，您将会得到两者之和/差/积/商");
+    scanf("%lf %lf",&a,&b);
+    printf("%lf+%lf=%lf\n",a,b,a+b);
+    printf("%lf-%lf=%lf\n",a,b,a-b);
+    printf("%lf*%lf=%lf\n",a,b,a*b);
+    printf("%lf/%lf=%lf\n",a,b,a/b);
     return 0;
 }
