608292576a1fb6ddc1e929c222e069fc7cd7e443
xizhe-wang <142483410+WangXizhe-dev@users.noreply.github.com>
2026-07-09 18:40:52 +0800

    Create CalculateChange.c

---
+++ 3.1If/CalculateChange.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int price = 0;
+    int bill = 0;
+    printf("请输入产品金额");
+    scanf("%d",&price);
+    printf("请输入支付票面");
+    scanf("%d",&bill);
+    if (price <= bill){
+        printf("找您%d元\n",bill-price);
+    }
+    else{
+        printf("您的钱不够\n");
+    }
+}
