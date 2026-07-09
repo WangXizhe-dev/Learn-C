a988b616ceb7c9e0a75b0dfd835a00ee14d0c3c1
xizhe-wang <142483410+WangXizhe-dev@users.noreply.github.com>
2026-07-09 19:52:18 +0800

    Create LeapYear.c

---
+++ 3.1If/LeapYear.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int year = 0;
+    printf("请输入一个年份");
+    scanf("%d",&year);
+    if ((year%4 ==0 && year%100 != 0) || (year%400 == 0)){
+        printf("这是一个闰年\n");
+    }
+    else{
+        printf("这不是一个闰年\n");
+    }
+    return 0;
+}
