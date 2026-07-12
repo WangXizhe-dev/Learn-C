47d4640c7734fce8388b3fa14881d297f0118d9f
xizhe-wang <142483410+WangXizhe-dev@users.noreply.github.com>
2026-07-12 15:21:54 +0800

    Create DigitCount1.0.c

---
+++ 4.1Loop/DigitCount1.0.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n = 0, num = 0,nums = 0;
+    printf("请输入一个正整数");
+    scanf("%d",&num);
+    nums = num;
+    n ++;
+    num /= 10;
+    while (num > 0){
+        n++;
+        num /= 10;
+    }
+    printf("%d是%d位数\n",nums,n);
+    
+    return 0;
+}
