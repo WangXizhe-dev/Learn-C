ed61866f166f57be1e3a794451dae3fb8412b3c6
xizhe-wang <142483410+WangXizhe-dev@users.noreply.github.com>
2026-07-13 18:54:53 +0800

    Create factorial.c

---
+++ 5.1LoopFor/factorial.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n = 0,ans = 1,i = 1;
+    printf("请输入一个正整数，系统将会计算阶乘");
+    scanf("%d",&n);
+    for (i = 1 ;i <= n ; i++){
+        ans *= i;
+    }
+    printf("%d的阶乘为%d\n",n,ans);
+    return 0;
+}
