3031f733e6bdd9238ec1a20ca0d034548188d4e8
xizhe-wang <142483410+WangXizhe-dev@users.noreply.github.com>
2026-07-13 19:21:45 +0800

    Update factorial2.0.c

--- 5.1LoopFor/factorial2.0.c
+++ 5.1LoopFor/factorial2.0.c
@@ -4,9 +4,14 @@ int main()
     int n = 0,ans = 1,i = 1;
     printf("请输入一个正整数，系统将会计算阶乘");
     scanf("%d",&n);
-    for (i = n ;i >1 ; i--){
-        ans *= i;
+    if (n < 20){
+        for (i = n ;i >1 ; i--){
+            ans *= i;
+        }
+        printf("%d的阶乘为%d\n",n,ans);
+    }else{
+        printf("数字过大\n");
     }
-    printf("%d的阶乘为%d\n",n,ans);
+    
     return 0;
 }
