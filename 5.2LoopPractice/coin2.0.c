ce14230f2083e86e00a55dd0ec7f8362588b6fe7
xizhe-wang <142483410+WangXizhe-dev@users.noreply.github.com>
2026-07-15 17:36:51 +0800

    Create coin2.0.c

---
+++ 5.2LoopPractice/coin2.0.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n = 0, one, two, five, exit = 0;
+    printf("请输入金额（元）");
+    scanf("%d",&n);
+    for (one = 0; one <= n * 10; one ++){
+        for (two = 0; two <= n * 5; two ++){
+            for (five = 0; five <= n * 2; five ++){
+                if (one + two * 2 + five * 5 == n * 10){
+                    printf("可以由%d个一角,%d个两角,%d个五角凑出%d元\n",one,two,five,n);
+                    exit = 1;
+                    break;
+                }
+            }
+        if (exit == 1){
+            break;
+        }
+    }
+    if (exit == 1){
+            break;
+        }
+}
+    return 0;
+}
