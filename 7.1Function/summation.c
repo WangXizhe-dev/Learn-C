08c3fc73bb7346bb6e6c5ddb801feb5f471db205
xizhe-wang <142483410+WangXizhe-dev@users.noreply.github.com>
2026-07-28 21:25:57 +0800

    Update summation.c

--- 7.1Function/summation.c
+++ 7.1Function/summation.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-
+void sum(int begin,int end);
+int main(){
+    sum(1,100);
+    return 0;
+}
 void sum(int begin,int end){
     int sum = 0;
     int i = begin;
@@ -7,8 +11,4 @@ void sum(int begin,int end){
         sum += i;
     }
     printf("%d到%d累加之和为%d\n",begin,end,sum);
-}
-int main(){
-    sum(1,100);
-    return 0;
 }
