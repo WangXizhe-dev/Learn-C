03852f20e1daf850da264c20f93b95fe35978654
xizhe-wang <142483410+WangXizhe-dev@users.noreply.github.com>
2026-07-28 20:59:30 +0800

    Create summation.c

---
+++ 7.1Function/summation.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void sum(int begin,int end){
+    int sum = 0;
+    int i = begin;
+    for(;i <= end;i++){
+        sum += i;
+    }
+    printf("%d到%d累加之和为%d\n",begin,end,sum);
+}
+int main(){
+    sum(1,100);
+    return 0;
+}
