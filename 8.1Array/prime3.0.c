8c8e06e70ae6a2dea098796cc83e6de8d24a2037
xizhe-wang <142483410+WangXizhe-dev@users.noreply.github.com>
2026-08-01 21:47:33 +0800

    Create prime3.0.c

---
+++ 8.1Array/prime3.0.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int isPrime(int n);
+
+int main(){
+    int n = 0;
+    printf("请输入一个正整数");
+    scanf("%d",&n);
+    if (isPrime(n)){
+        printf("是质数\n");
+    }else{
+        printf("不是质数\n");
+    }
+    return 0;
+}
+
+int isPrime(int n){
+    int prime[100] = {2,3};
+    int isPrime = 1;
+    int i = 2;
+    if (n == 1){
+        isPrime = 0;
+    }
+    for (;i*i <= n;i++){
+        if (n % i == 0){
+            isPrime = 0;
+            break;
+        }
+    }
+    return isPrime;
+
+}
