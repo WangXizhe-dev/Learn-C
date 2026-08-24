6c77aba21e1bad4a693d1fa1fe6bf561d5c7b1a4
xizhe-wang <142483410+WangXizhe-dev@users.noreply.github.com>
2026-08-24 21:47:09 +0800

    Create prime4.0.c

---
+++ 8.1Array/prime4.0.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+const int number = 100;
+int main(){
+    int prime[number];
+    int i = 0;
+    for(;i < number;i++){
+        prime[i] = 1;
+    }
+    //啊啊啊啊啊啊还没想清楚
+    for(i = 0;i <number;i++){
+
+    }
+    return 0;
+}
