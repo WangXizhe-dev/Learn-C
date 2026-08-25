48d634b62af9a43203b18c7078236960f0d16081
xizhe-wang <142483410+WangXizhe-dev@users.noreply.github.com>
2026-08-25 21:44:54 +0800

    Update prime4.0.c

--- 8.1Array/prime4.0.c
+++ 8.1Array/prime4.0.c
@@ -2,14 +2,24 @@
 
 const int number = 100;
 int main(){
-    int prime[number];
-    int i = 0;
+    int prime[number+1];
+    int i = 2;
     for(;i < number;i++){
         prime[i] = 1;
     }
-    //啊啊啊啊啊啊还没想清楚
-    for(i = 0;i <number;i++){
-
+    int j;
+    for(i = 2;i*i <number;i++){
+        if (prime[i] == 1){
+            for(j = 2;j*i <= number;j++){
+                prime[j*i] = 0;
+            }
+        }
     }
+    for (i = 2;i < number;i++){
+        if (prime[i] == 1){
+            printf("%d ",i);
+        }
+    }
+    printf("\n");
     return 0;
 }
