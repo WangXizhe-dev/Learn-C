0c710faa9c8c3692a32d1822e6af3da791cba5c5
xizhe-wang <142483410+WangXizhe-dev@users.noreply.github.com>
2026-08-27 20:58:13 +0800

    Update binarySearch.c

--- 8.1Array/binarySearch.c
+++ 8.1Array/binarySearch.c
@@ -1,16 +1,23 @@
 #include<stdio.h>
 int main(){
-    int array[] = {1,3,4,5,6,7,10};
-    const int target = 7;
-    int len = sizeof(array) / sizeof(array[0]);
-    int i = len / 2 ;
-    while (array[i] != target){
-        if (array[i] > target){
-            i = (i + 1)/2 ;
+    int arr[] = {1,3,4,5,6,7,10};
+    const int target = 3;
+    int len = sizeof(arr) / sizeof(arr[0]);
+    int left = 0,right = len - 1;
+    int ret = -1;
+    int mid;
+    while (left <= right){
+        mid = (left+right)/2;
+        if (arr[mid] == target){
+            ret = mid;
+            break;
+        }else if(arr[mid] > target){
+            right = mid -1;
         }else{
-            i = i + (len - i - 1) ;
+            left = mid + 1;
         }
     }
-    printf("%d在第%d号\n",target,i+1);
+    printf("目标元素的索引：%d\n",ret);
+    
     return 0;
 }
