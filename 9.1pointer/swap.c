7651800f490acd0a91f6b5592ac068228f32227e
xizhe-wang <142483410+WangXizhe-dev@users.noreply.github.com>
2026-08-28 21:28:24 +0800

    Create swap.c

---
+++ 9.1pointer/swap.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void swap(int *p1,int *p2);
+
+int main(){
+    int a = 1;
+    int b = 2;
+    swap(&a,&b);
+    printf("a=%d,b=%d\n",a,b);
+    return 0;
+}
+
+void swap(int *p1,int *p2){
+    int t = *p1;
+    *p1 = *p2;
+    *p2 = t;
+}
