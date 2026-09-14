5aaee07a82d9af4075d529e8355d7cb2384113ac
xizhe-wang <142483410+WangXizhe-dev@users.noreply.github.com>
2026-09-14 23:48:20 +0800

    Create reverse.c

---
+++ 9.1pointer/reverse.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void reverse(int *arr,int n);
+
+int main(){
+    int arr[] = {1,3,4,5,8};
+    int len = sizeof(arr)/sizeof(arr[0]);
+    reverse(arr,len);
+    for (int i = 0 ;i<len;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+void reverse(int *arr,int n){
+    for(int i = 0;i<(n+n%2)/2;i++){
+        int t = *(arr + i);
+        *(arr + i) = *(arr + n - i - 1) ;
+        *(arr + n - i - 1) = t;
+    }
+}
